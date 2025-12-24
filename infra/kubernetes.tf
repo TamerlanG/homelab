ac5ae9aca973eaabe93542e3584141578de5f6df
Tamerlan Gudabayev <tamerlanalbehairy@gmail.com>
2025-12-24 20:18:07 +0100

    feat: tailscale operator

--- infra/kubernetes.tf
+++ infra/kubernetes.tf
@@ -8,10 +8,41 @@ resource "kubernetes_namespace" "argocd" {
 }
 
 
+resource "kubernetes_namespace" "tailscale" {
+  metadata {
+    name = "tailscale"
+  }
+  depends_on = [
+    ansible_playbook.playbook_configure,
+  ]
+}
+
+
+
 resource "helm_release" "argocd" {
   name       = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
   namespace  = kubernetes_namespace.argocd.metadata[0].name
   version    = "9.1.9"
+}
+
+resource "helm_release" "tailscale_operator" {
+  name       = "tailscale-operator"
+  repository = "https://pkgs.tailscale.com/helmcharts"
+  chart      = "tailscale-operator"
+  namespace  = kubernetes_namespace.tailscale.metadata[0].name
+  version    = "1.92.3"
+  set = [
+    {
+      name  = "oauth.clientId"
+      value = var.tailscale_oauth_client_id
+      type  = "string"
+    },
+    {
+      name  = "oauth.clientSecret"
+      value = var.tailscale_oauth_client_secret
+      type  = "string"
+    }
+  ]
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -2,6 +2,14 @@ variable "hcloud_token" {
   sensitive = true
 }
 
+variable "tailscale_oauth_client_id" {
+  sensitive = true
+}
+
+variable "tailscale_oauth_client_secret" {
+  sensitive = true
+}
+
 variable "argocd_repo_url" {
   description = "Git repo URL for Argo CD applications (SSH or HTTPS)"
   type        = string
